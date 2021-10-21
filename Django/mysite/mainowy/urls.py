5cfc5dbe6720753b19810229a0e85c6311bd54e1
unknown <rewasek@gmail.com>
2021-10-21 18:24:12 +0200

    a

--- Django/mysite/mainowy/urls.py
+++ Django/mysite/mainowy/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("v1/", views.v1, name="index"),
     path("v2/", views.v2, name="index"),
+    path("v2/", views.v2, name="index"),
 ]
